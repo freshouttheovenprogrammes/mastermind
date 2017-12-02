3e5ed9b8621005c719a7f0a310cb41f6963fd358
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-02 15:37:46 -0700

    Added super-testing abilities

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'minitest'
+gem 'pry'
+gem 'rake'
+gem 'reek'
+gem 'cane'
+
+gem 'simplecov', :require => false, :group => :test
--- test/game_manager_test.rb
+++ test/game_manager_test.rb
@@ -1,5 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require_relative 'test_helper'
 require './lib/game_manager'
 
 class GameManagerTest < Minitest::Test
