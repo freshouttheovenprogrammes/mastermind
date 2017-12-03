b9971af4b5ec146b4cc58b2b259f3489d9a22168
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-03 16:52:59 -0700

    going to create new branch

--- lib/mastermind_runner_control.rb
+++ lib/mastermind_runner_control.rb
@@ -1,10 +1,20 @@
-require_relative './lib/game_prompts'
-require_relative './lib/game_manager'
+require_relative 'game_prompts'
+require_relative 'game_manager'
+require_relative 'guess_manager'
 
 class MastermindRunnerControl
+  attr_reader :game_prompts,
+              :guess_manager,
+              :game_manager
 
   def initialize
-    @game_prompts = GamePrompts.new
+    @game_prompts  = GamePrompts.new
+    @guess_manager = GuessManager.new
+    @game_manager  = GameManager.new
+  end
+
+  def play_prompt
+    game_prompts.play_prompt
   end
 
 
--- test/mastermind_runner_test.rb
+++ test/mastermind_runner_test.rb
@@ -1,12 +1,38 @@
 require_relative 'test_helper'
-require '../mastermind/mastermind'
+require './lib/mastermind_runner_control'
 
-class MastermindTest < Minitest::Test
-  # 
-  # def test_that_it_exists
-  #   master_mind = Mastermind.new
-  #
-  #   assert_instance_of Mastermind, master_mind
-  # end
+class MastermindRunnerControlTest < Minitest::Test
+
+  def test_that_it_exists
+    runner = MastermindRunnerControl.new
+
+    assert_instance_of MastermindRunnerControl, runner
+  end
+
+  def test_that_it_can_reach_game_prompts
+    runner = MastermindRunnerControl.new
+
+    assert_equal GamePrompts, runner.game_prompts.class
+  end
+
+  def test_that_it_can_reach_guess_class
+    runner = MastermindRunnerControl.new
+
+    assert_equal GuessManager, runner.guess_manager.class
+  end
+
+  def test_that_it_can_reach_gamemanager_class
+    runner = MastermindRunnerControl.new
+
+    assert_equal GameManager, runner.game_manager.class
+  end
+
+  def test_user_play_prompt_is_successfully_hit_upon_p_input
+    skip
+    runner = MastermindRunnerControl.new
+    runner.guess_manager.user_input("p")
+    assert_equal "I have generated a sequence with four elements made up of: (r)ed,
+  (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.", runner.game_prompts.play_prompt
+  end
 
 end
